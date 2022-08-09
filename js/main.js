a60276e98683d777bba6eb349f27a95046657025
Rejoice2374 <adeboyerejoice2374@gmail.com>
2022-08-10 00:07:50 +0100

    first update
    the first line of code

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// change navbar styles on scroll
+
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 20)
+})
+
+
+
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus';
+        } else {
+            icon.className = 'uil uil-plus';
+        }
+    })
+})
+
+
+// swipperjs
+
+
+var swiper = new Swiper(".mySwiper", {slidesPerView: "1", spaceBetween: 30, pagination: {el: ".swiper-pagination", clickable: true,},
+// when window width is >+ 600px
+breakpoints: (
+    600: {
+        slidesPerView:2
+    }
+)
+});
+  

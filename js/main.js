3785b4c71f0a9ed9c8249b2904c2743a27aa9fd1
Rejoice2374 <adeboyerejoice2374@gmail.com>
2022-08-11 01:49:17 +0100

    6th update including media querry for tabs & phones

--- js/main.js
+++ js/main.js
@@ -27,3 +27,24 @@ faqs.forEach(faq => {
 })
 
 
+// show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+const openNav = () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+}
+
+menuBtn.addEventListener('click', openNav
+)
+
+const closeNav = () => {
+    menu.style.display = "none";
+    menuBtn.style.display = "inline-block";
+    closeBtn.style.display = "none";
+}
+
+closeBtn.addEventListener('click', closeNav)
